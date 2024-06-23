d2eafe3046439ee6ec5c034f576fbf13617b6cca
Chitraksh Tarun <65770420+ChitrakshTarun@users.noreply.github.com>
2024-06-24 00:03:09 +0530

    Code cleanup and commenting, Update README.md

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { TouchableOpacity, StyleSheet, Text, View, Alert } from "react-native";
 import { useCallback, useEffect, useState } from "react";
-import { Slider } from "@miblanchard/react-native-slider";
-import Checkbox from "expo-checkbox";
+import { TouchableOpacity, StyleSheet, Text, View, Alert } from "react-native";
 import * as Clipboard from "expo-clipboard";
+import Checkbox from "expo-checkbox";
+import { Slider } from "@miblanchard/react-native-slider";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+	// SET STATES TO MANAGE PASSWORD CONTROLS
 	const [password, setPassword] = useState<string>("");
 	const [length, setLength] = useState(8);
 	const [allowNumbers, setAllowNumbers] = useState<boolean>(true);
 	const [allowSpecialCharacters, setAllowSpecialCharacters] = useState<boolean>(true);
 
+	// FUNCTION - GENERATE PASSWORD
 	const generatePassword = useCallback(() => {
 		let generatedPassword: string = "";
 
@@ -21,30 +23,36 @@ export default function App() {
 		for (let i = 0; i < length; i++) {
 			generatedPassword += allowedCharacters.charAt(Math.floor(Math.random() * allowedCharacters.length + 1));
 		}
-
 		setPassword(generatedPassword);
 	}, [length, allowNumbers, allowSpecialCharacters]);
 
+	// FUNCTION - COPY PASSWORD TO CLIPBOARD
 	const copyPassword = async () => {
 		await Clipboard.setStringAsync(password);
 		Alert.alert("Password Copied!");
 	};
 
+	// USEEFFECT TO RE-RUN PASSWORD GENERATOR WHEN ANY CONTROL IS CHANGED
 	useEffect(() => {
 		generatePassword();
 	}, [length, allowNumbers, allowSpecialCharacters]);
 
 	return (
 		<View style={styles.container}>
+			<StatusBar style="auto" />
+			{/* PASSWORD CONTAINER */}
 			<View style={styles.passwordContainer}>
+				{/* HEADING & PASSWORD */}
 				<Text style={styles.headingText}>Your password is: </Text>
 				<Text style={styles.passwordText}>{password}</Text>
+				{/* BUTTON - GENERATE PASSWORD */}
 				<TouchableOpacity
 					style={styles.buttonContainer}
 					onPress={generatePassword}
 				>
 					<Text style={styles.headingText}>Generate Password</Text>
 				</TouchableOpacity>
+				{/* BUTTON - COPY PASSWORD */}
 				<TouchableOpacity
 					style={styles.buttonContainer}
 					onPress={copyPassword}
@@ -52,9 +60,10 @@ export default function App() {
 					<Text style={styles.headingText}>Copy Password</Text>
 				</TouchableOpacity>
 			</View>
-			<StatusBar style="auto" />
+			{/* CONTROLS CONTAINER */}
 			<View style={styles.controlsMenuContainer}>
 				<Text style={styles.headingText}>Controls</Text>
+				{/* PASSWORD LENGTH */}
 				<Text style={styles.controlsText}>Password Length: {length}</Text>
 				<Slider
 					value={length}
@@ -64,6 +73,7 @@ export default function App() {
 					onValueChange={(value) => setLength(value[0])}
 					trackClickable={true}
 				/>
+				{/* PASSWORD ALLOW NUMBERS */}
 				<View style={styles.controlsContainer}>
 					<Checkbox
 						style={styles.checkbox}
@@ -73,6 +83,7 @@ export default function App() {
 					/>
 					<Text style={styles.controlsText}>Allow Numbers (0 1 2 3 4 5 6 7 8 9)</Text>
 				</View>
+				{/* PASSWORD ALLOW CHARACTERS */}
 				<View style={styles.controlsContainer}>
 					<Checkbox
 						style={styles.checkbox}
@@ -94,13 +105,6 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		justifyContent: "center",
 	},
-	headingText: {
-		fontSize: 24,
-		paddingBottom: 8,
-	},
-	passwordText: {
-		fontSize: 36,
-	},
 	passwordContainer: {
 		flex: 2,
 		alignItems: "center",
@@ -119,12 +123,6 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		alignItems: "center",
 	},
-	controlsText: {
-		fontSize: 16,
-	},
-	checkbox: {
-		margin: 8,
-	},
 	buttonContainer: {
 		backgroundColor: "#55a0e0",
 		padding: 12,
@@ -133,5 +131,18 @@ const styles = StyleSheet.create({
 		borderRadius: 16,
 		justifyContent: "center",
 		alignItems: "center",
+	},
+	headingText: {
+		fontSize: 24,
+		paddingBottom: 8,
+	},
+	passwordText: {
+		fontSize: 36,
+	},
+	controlsText: {
+		fontSize: 16,
+	},
+	checkbox: {
+		margin: 8,
 	},
 });
---
+++ README.md
@@ -0,0 +1,53 @@
+# Magic 8 Ball
+
+A simple single page password generator made using React Native and Expo.
+
+# Tech Stack
+
+### Frameworks, Tools & Languages
+
+- React Native
+- Expo
+- JavaScript/TypeScript
+
+### Dependencies
+
+- `expo-checkbox` Toggle for allowing numbers or characters in Password
+- `expo-clipboard` Copying Password to clipboard
+- `@miblanchard/react-native-slider` Slider component to change Password Length
+
+# Run Locally
+
+### Pre-requisites
+
+- Node.js
+- Expo CLI
+- Android/iOS Emulator (A Physical Device works as well)
+
+Considering the dependencies being used in this Expo project, you should be able to run the app using Expo Go.
+
+### Installation
+
+1. Clone this Repository, then change directory to the HexSplash directory.
+
+```
+https://github.com/ChitrakshTarun/PasswordCipher.git
+```
+
+```
+cd PasswordCipher
+```
+
+2. Install Dependencies
+
+```
+npm install
+```
+
+3.
+
+```
+npx expo start
+```
+
+Either scan the QR code using Expo Go on your device, or use an Android/IOS emulator with Expo Go.
